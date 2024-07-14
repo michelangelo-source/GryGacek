f04d2a48c1df4ff0d1d7bb306d7d4036e1acc3ae
TomaszGacek <gacektomasz02@gmail.com>
2024-07-14 13:47:17 +0200

    sttt board

--- front/GryGacek/src/app/gamesComponents/sttt/sttt.component.ts
+++ front/GryGacek/src/app/gamesComponents/sttt/sttt.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sttt.component.html',
-  styleUrl: './sttt.component.scss'
+  styleUrl: './sttt.component.scss',
 })
 export class StttComponent {
+  boards_table: number[][] = [];
+  ngOnInit() {
+    for (let i = 0; i < 9; i++) {
+      this.boards_table.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+  }
 
+  add_chart(iid: number, jid: number) {
+    alert(iid + ' ' + jid);
+  }
 }
