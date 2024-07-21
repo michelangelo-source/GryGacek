2e0131acf8ea6d073dbfc08a711f225ecc71eaae
TomaszGacek <gacektomasz02@gmail.com>
2024-07-21 14:02:28 +0200

    blocks falls in tetris

--- front/GryGacek/src/app/gamesComponents/tetris/tetris.component.ts
+++ front/GryGacek/src/app/gamesComponents/tetris/tetris.component.ts
@@ -12,9 +12,19 @@ export type cellTetris = {
   styleUrl: './tetris.component.scss',
 })
 export class TetrisComponent {
+  falling_interval: NodeJS.Timeout = setTimeout(() => {}, 0);
   board: cellTetris[][] = [];
-
-  /////////dopisac dwie 2 puste u gory tam sie respi block i spada po can_fall
+  // let color: string[] = [
+  //   'blue',
+  //   'green',
+  //   'red',
+  //   'yellow',
+  //   'purple',
+  //   'orange',
+  //   'navy',
+  // ];
+  ///////// block i spada po can_fall
+  //w boki po kolumnach push pop
 
   ngOnInit() {
     let row: cellTetris[] = [];
@@ -30,57 +40,112 @@ export class TetrisComponent {
     }
     row = [];
     for (let j = 0; j < 12; j++) {
-      row.push({ color: 'black', occupied: false, can_fall: false });
+      row.push({ color: 'black', occupied: true, can_fall: false });
     }
     this.board.push(row);
-    setTimeout(() => {}, 1000);
     this.nextblock();
   }
   gameover() {
     alert('dupa');
   }
   nextblock() {
-    if (this.board[0][5].color == 'white') {
-      let can_blacok_fall = true;
-    } else {
+    if (this.board[0][5].color != 'white') {
       this.gameover();
+      return;
     }
     let block = Math.floor(Math.random() * 7);
-    this.fall(block);
-    // switch (block) {
-    //   case 0:
-    //     this.board[0][5].color = 'blue';
-    //     break;
-    //   case 1:
-    //     this.board[0][5].color = 'green';
-    //     break;
-    //   case 2:
-    //     this.board[0][5].color = 'red';
-    //     break;
-    //   case 3:
-    //     this.board[0][5].color = 'yellow';
-    //     break;
-    //   case 4:
-    //     this.board[0][5].color = 'purple';
-    //     break;
-    //   case 5:
-    //     this.board[0][5].color = 'orange';
-    //     break;
-    //   case 6:
-    //     this.board[0][5].color = 'navy';
-    //     break;
-    // }
+
+    switch (block) {
+      case 0:
+        this.board[0][7] = { color: 'blue', occupied: true, can_fall: true };
+        this.board[0][4] = { color: 'blue', occupied: true, can_fall: true };
+        this.board[0][5] = { color: 'blue', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'blue', occupied: true, can_fall: true };
+
+        break;
+      case 1:
+        this.board[0][5] = { color: 'green', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'green', occupied: true, can_fall: true };
+        this.board[1][5] = { color: 'green', occupied: true, can_fall: true };
+        this.board[1][4] = { color: 'green', occupied: true, can_fall: true };
+
+        break;
+      case 2:
+        this.board[0][5] = { color: 'red', occupied: true, can_fall: true };
+        this.board[0][4] = { color: 'red', occupied: true, can_fall: true };
+        this.board[1][5] = { color: 'red', occupied: true, can_fall: true };
+        this.board[1][6] = { color: 'red', occupied: true, can_fall: true };
+        break;
+      case 3:
+        this.board[0][5] = { color: 'yellow', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'yellow', occupied: true, can_fall: true };
+        this.board[1][5] = { color: 'yellow', occupied: true, can_fall: true };
+        this.board[1][6] = { color: 'yellow', occupied: true, can_fall: true };
+        break;
+      case 4:
+        this.board[0][5] = { color: 'purple', occupied: true, can_fall: true };
+        this.board[0][4] = { color: 'purple', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'purple', occupied: true, can_fall: true };
+        this.board[1][5] = { color: 'purple', occupied: true, can_fall: true };
+        break;
+      case 5:
+        this.board[0][5] = { color: 'orange', occupied: true, can_fall: true };
+        this.board[0][4] = { color: 'orange', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'orange', occupied: true, can_fall: true };
+        this.board[1][6] = { color: 'orange', occupied: true, can_fall: true };
+        break;
+      case 6:
+        this.board[0][5] = { color: 'navy', occupied: true, can_fall: true };
+        this.board[0][6] = { color: 'navy', occupied: true, can_fall: true };
+        this.board[0][4] = { color: 'navy', occupied: true, can_fall: true };
+        this.board[1][4] = { color: 'navy', occupied: true, can_fall: true };
+        break;
+    }
+
+    this.falling_interval = setInterval(() => this.fall(), 200);
   }
-  fall(block: number) {
-    let color: string[] = [
-      'blue',
-      'green',
-      'red',
-      'yellow',
-      'purple',
-      'orange',
-      'navy',
-    ];
-    this.board[0][5].color = color[block];
+  stand() {
+    this.board.forEach((element) => {
+      element.forEach((el) => {
+        el.can_fall = false;
+      });
+    });
+  }
+  fall() {
+    for (let i = 19; i >= 0; i--) {
+      for (let j = 1; j <= 10; j++) {
+        if (
+          this.board[i][j].can_fall == true &&
+          this.board[i + 1][j].occupied == true &&
+          this.board[i + 1][j].can_fall == false
+        ) {
+          this.stand();
+          this.nextblock();
+          clearInterval(this.falling_interval);
+          return false;
+        }
+      }
+    }
+
+    for (let i = 19; i >= 0; i--) {
+      for (let j = 1; j <= 10; j++) {
+        if (this.board[i][j].can_fall == true) {
+          this.board[i + 1][j] = this.board[i][j];
+          this.board[i][j] = {
+            color: 'white',
+            occupied: false,
+            can_fall: false,
+          };
+        }
+      }
+    }
+    return true;
+  }
+  move(dircetion: boolean) {
+    if (dircetion) {
+      console.log('prawo');
+    } else {
+      console.log('lewo');
+    }
   }
 }
