ee513fb87880f9da49e25f97d22f3f8f16073530
TomaszGacek <gacektomasz02@gmail.com>
2024-08-08 13:55:20 +0200

    kulki on progres

--- front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
+++ front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
@@ -4,6 +4,7 @@ interface squere {
   color: string;
   x: number;
   y: number;
+  is_selected: boolean;
 }
 
 @Component({
@@ -14,6 +15,16 @@ interface squere {
   styleUrl: './kulki.component.scss',
 })
 export class KulkiComponent {
+  navy_path: string = '/assets/images/kulki/navy.png';
+  blue_path: string = '/assets/images/kulki/blue.png';
+  green_path: string = '/assets/images/kulki/green.png';
+  red_path: string = '/assets/images/kulki/red.png';
+  yellow_path: string = '/assets/images/kulki/yellow.png';
+  purple_path: string = '/assets/images/kulki/purple.png';
+  orange_path: string = '/assets/images/kulki/orange.png';
+  white_path: string = '/assets/images/kulki/white.png';
+  selected_interval: NodeJS.Timeout = setTimeout(() => {}, 0);
+  selected_squere: squere = { x: 9, y: 9, color: '', is_selected: false };
   board: squere[][] = [];
   empty: squere[] = [];
   colors: string[] = [
@@ -34,21 +45,57 @@ export class KulkiComponent {
           color: 'black',
           x: i,
           y: j,
+          is_selected: false,
         });
         this.empty.push({
           color: 'black',
           x: i,
           y: j,
+          is_selected: false,
         });
       }
       this.board.push(row);
     }
-    while (this.empty.length > 1) {
-      this.add_balls();
-    }
+
+    this.add_balls();
+    this.add_balls();
+    this.add_balls();
+    this.add_balls();
+    this.add_balls();
+    this.add_balls();
   }
-  chose_ball(x: number, y: number) {
-    alert('x: ' + x + ' y:' + y);
+  chose_ball(selected_squre: squere) {
+    if (selected_squre.color != 'black') {
+      if (
+        selected_squre.x == this.selected_squere.x &&
+        selected_squre.x == this.selected_squere.y
+      ) {
+        ///////////////////////////////////////////////////////////////do tegho ifa nie wchodzi odklikanie
+
+        clearInterval(this.selected_interval);
+        selected_squre.is_selected = false;
+      } else {
+        clearInterval(this.selected_interval);
+        if (this.selected_squere.x != 9) {
+          this.board[this.selected_squere.x][
+            this.selected_squere.y
+          ].is_selected = false;
+        }
+        this.selected_squere = selected_squre;
+        selected_squre.is_selected = !selected_squre.is_selected;
+        this.selected_interval = setInterval(() => {
+          selected_squre.is_selected = !selected_squre.is_selected;
+        }, 500);
+      }
+    } else {
+      alert('czarne');
+    }
+
+    //html dla wszystkich kolorów
+
+    //ruch
+    //sprawdzanie 5 i wiećej
+    //pkt
   }
   add_balls() {
     if (this.empty.length < 3) {
