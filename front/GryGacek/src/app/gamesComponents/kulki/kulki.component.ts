70a4c61b6d403d0b087e293565d3c95613128cff
TomaszGacek <gacektomasz02@gmail.com>
2024-08-13 13:54:08 +0200

    fighting with a*

--- front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
+++ front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
@@ -1,10 +1,19 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
+import path from 'node:path';
 
 export type squere = {
   color: string;
   x: number;
   y: number;
   is_selected: boolean;
+};
+export type path_node = {
+  cost: number;
+  prev?: path_node;
+  x: number;
+  y: number;
+  visited: boolean;
 };
 
 @Component({
@@ -38,7 +47,9 @@ export class KulkiComponent {
     'orange',
     'navy',
   ];
-
+  pythagoras(x1: number, y1: number, x2: number, y2: number) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  }
   ngOnInit() {
     for (let i = 0; i < 9; i++) {
       let row: squere[] = [];
@@ -89,20 +100,194 @@ export class KulkiComponent {
         this.board[this.selected_squere.x][this.selected_squere.y].is_selected =
           false;
 
-        this.find_path(selected_squre);
+        console.log(this.find_path(selected_squre));
       }
     }
   }
+
   find_path(destination: squere) {
+    ///not working
     console.log(this.selected_squere);
     console.log(destination);
+
+    let start: path_node = {
+      visited: false,
+      x: this.selected_squere.x,
+      y: this.selected_squere.y,
+      cost: 999,
+    };
+    let visited: path_node[] = [start];
+    console.log(visited);
+    let i = 0;
+
+    while (i < 81) {
+      let elem = visited[0];
+      for (let i = 0; i < visited.length - 1; i++) {
+        if (!visited[i].visited) {
+          elem = visited[i];
+          visited[i].visited = true;
+        }
+      }
+
+      if (elem.x <= 7 && this.board[elem.x + 1][elem.y].color == 'black') {
+        let can_create = true;
+
+        for (let i = 0; i < visited.length - 1; i++) {
+          if (visited[i].x == elem.x + 1 && visited[i].y == elem.y) {
+            can_create = false;
+          }
+        }
+
+        if (can_create) {
+          let new_node: path_node = {
+            visited: false,
+            prev: elem,
+            x: elem.x + 1,
+            y: elem.y,
+            cost:
+              this.pythagoras(
+                elem.x + 1,
+                elem.y,
+                destination.x,
+                destination.y
+              ) + this.cost_counter(elem),
+          };
+
+          if (new_node.x == destination.x && new_node.y == destination.y) {
+            visited.unshift(new_node);
+            break;
+          } else {
+            visited.push(new_node);
+            visited.sort((a, b) => {
+              return a.cost - b.cost;
+            });
+          }
+        }
+      }
+      if (elem.x >= 1 && this.board[elem.x - 1][elem.y].color == 'black') {
+        let can_create = true;
+        for (let i = 0; i < visited.length - 1; i++) {
+          if (visited[i].x == elem.x - 1 && visited[i].y == elem.y) {
+            can_create = false;
+          }
+        }
+        if (can_create) {
+          let new_node: path_node = {
+            visited: false,
+            prev: elem,
+            x: elem.x - 1,
+            y: elem.y,
+            cost:
+              this.pythagoras(
+                elem.x - 1,
+                elem.y,
+                destination.x,
+                destination.y
+              ) + this.cost_counter(elem),
+          };
+
+          if (new_node.x == destination.x && new_node.y == destination.y) {
+            visited.unshift(new_node);
+            break;
+          } else {
+            visited.push(new_node);
+            visited.sort((a, b) => {
+              return a.cost - b.cost;
+            });
+          }
+        }
+      }
+      if (elem.y <= 7 && this.board[elem.x][elem.y + 1].color == 'black') {
+        let can_create = true;
+        for (let i = 0; i < visited.length - 1; i++) {
+          if (visited[i].x == elem.x && visited[i].y == elem.y + 1) {
+            can_create = false;
+          }
+        }
+        if (can_create) {
+          let new_node: path_node = {
+            visited: false,
+            prev: elem,
+            x: elem.x,
+            y: elem.y + 1,
+            cost:
+              this.pythagoras(
+                elem.x,
+                elem.y + 1,
+                destination.x,
+                destination.y
+              ) + this.cost_counter(elem),
+          };
+
+          if (new_node.x == destination.x && new_node.y == destination.y) {
+            visited.unshift(new_node);
+            break;
+          } else {
+            visited.push(new_node);
+            visited.sort((a, b) => {
+              return a.cost - b.cost;
+            });
+          }
+        }
+      }
+      if (elem.y >= 1 && this.board[elem.x][elem.y - 1].color == 'black') {
+        let can_create = true;
+        for (let i = 0; i < visited.length - 1; i++) {
+          if (visited[i].x == elem.x && visited[i].y == elem.y - 1) {
+            can_create = false;
+          }
+        }
+        if (can_create) {
+          let new_node: path_node = {
+            visited: false,
+            prev: elem,
+            x: elem.x,
+            y: elem.y - 1,
+            cost:
+              this.pythagoras(
+                elem.x,
+                elem.y - 1,
+                destination.x,
+                destination.y
+              ) + this.cost_counter(elem),
+          };
+
+          if (new_node.x == destination.x && new_node.y == destination.y) {
+            visited.unshift(new_node);
+            break;
+          } else {
+            visited.push(new_node);
+            visited.sort((a, b) => {
+              return a.cost - b.cost;
+            });
+          }
+        }
+      }
+
+      i++;
+    }
     this.selected_squere = { x: 9, y: 9, color: '', is_selected: false };
-    this.check();
+
+    if (i == 81) {
+      return 'dupa';
+    } else {
+      this.check();
+      return visited[0];
+    }
+  }
+  cost_counter(elem: path_node) {
+    let cost = 0;
+    while (elem.cost != 999) {
+      cost += elem.cost;
+      if (elem.prev) elem = elem.prev;
+    }
+    return cost;
   }
   check() {
+    console.log('check');
     //krzyz w srodku
     //4tablice rzedzy kolumny skosy rosnoce malejace
-    this.add_balls();
+    //  this.add_balls();
   }
   add_balls() {
     if (this.empty.length <= 3) {
