54c6a369d803d2d6bbd344275202ce92f9e229c6
TomaszGacek <gacektomasz02@gmail.com>
2024-08-17 15:16:57 +0200

    animation works,checking row and column works

--- front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
+++ front/GryGacek/src/app/gamesComponents/kulki/kulki.component.ts
@@ -43,6 +43,7 @@ export class KulkiComponent {
   };
   selected_interval: NodeJS.Timeout = setTimeout(() => {}, 0);
   animation_interval: NodeJS.Timeout = setTimeout(() => {}, 0);
+  is_animation_on_going: boolean = false;
   selected_squere: squere = {
     x: 9,
     y: 9,
@@ -50,6 +51,7 @@ export class KulkiComponent {
     is_selected: false,
     is_path: false,
   };
+  points = 0;
   board: squere[][] = [];
   empty: squere[] = [];
   colors: string[] = [
@@ -94,57 +96,88 @@ export class KulkiComponent {
     return this.paths[color];
   }
   chose_ball(selected_squre: squere) {
-    if (selected_squre.color != 'black') {
-      if (
-        selected_squre.x == this.selected_squere.x &&
-        selected_squre.y == this.selected_squere.y
-      ) {
-        clearInterval(this.selected_interval);
-        selected_squre.is_selected = false;
-        this.selected_squere = {
-          x: 9,
-          y: 9,
-          color: '',
-          is_selected: false,
-          is_path: false,
-        };
+    if (!this.is_animation_on_going) {
+      if (selected_squre.color != 'black') {
+        if (
+          selected_squre.x == this.selected_squere.x &&
+          selected_squre.y == this.selected_squere.y
+        ) {
+          clearInterval(this.selected_interval);
+          selected_squre.is_selected = false;
+          this.selected_squere = {
+            x: 9,
+            y: 9,
+            color: '',
+            is_selected: false,
+            is_path: false,
+          };
+        } else {
+          clearInterval(this.selected_interval);
+          if (this.selected_squere.x != 9) {
+            this.board[this.selected_squere.x][
+              this.selected_squere.y
+            ].is_selected = false;
+          }
+          this.selected_squere = selected_squre;
+          selected_squre.is_selected = !selected_squre.is_selected;
+          this.selected_interval = setInterval(() => {
+            selected_squre.is_selected = !selected_squre.is_selected;
+          }, 500);
+        }
       } else {
-        clearInterval(this.selected_interval);
         if (this.selected_squere.x != 9) {
+          clearInterval(this.selected_interval);
           this.board[this.selected_squere.x][
             this.selected_squere.y
           ].is_selected = false;
-        }
-        this.selected_squere = selected_squre;
-        selected_squre.is_selected = !selected_squre.is_selected;
-        this.selected_interval = setInterval(() => {
-          selected_squre.is_selected = !selected_squre.is_selected;
-        }, 500);
-      }
-    } else {
-      if (this.selected_squere.x != 9) {
-        clearInterval(this.selected_interval);
-        this.board[this.selected_squere.x][this.selected_squere.y].is_selected =
-          false;
-        let path_elem = this.find_path(selected_squre);
-        console.log(path_elem);
-        if (path_elem.cost != 999) {
-          let path: path_node[] = [];
-          while (path_elem.prev) {
+          let path_elem = this.find_path(selected_squre);
+          if (path_elem.cost != 999) {
+            let path: path_node[] = [];
+            while (path_elem.prev) {
+              this.board[path_elem.x][path_elem.y].is_path = true;
+              path.unshift(path_elem);
+              path_elem = path_elem.prev;
+            }
             this.board[path_elem.x][path_elem.y].is_path = true;
             path.unshift(path_elem);
-            path_elem;
-            path_elem = path_elem.prev;
+            let i = 0;
+            this.is_animation_on_going = true;
+            this.animation_interval = setInterval(() => {
+              this.ball_step(
+                path[i].x,
+                path[i].y,
+                path[i + 1].x,
+                path[i + 1].y
+              );
+              i++;
+              if (i == path.length - 1) {
+                clearInterval(this.animation_interval);
+                this.board[path[i].x][path[i].y].is_path = false;
+                for (let j = 0; j < this.empty.length; j++) {
+                  if (
+                    this.empty[j].x == path[path.length - 1].x &&
+                    this.empty[j].y == path[path.length - 1].y
+                  ) {
+                    this.empty[j] = {
+                      color: 'black',
+                      x: path[0].x,
+                      y: path[0].y,
+                      is_selected: false,
+                      is_path: false,
+                    };
+                    break;
+                  }
+                }
+                this.is_animation_on_going = false;
+                this.check();
+              }
+            }, 200);
+          } else {
+            this.board[path_elem.x][path_elem.y].is_path = true;
+            setTimeout(() => {
+              this.board[path_elem.x][path_elem.y].is_path = false;
+            }, 200);
           }
-          let i = 0;
-          // this.ball_step(path[i].x, path[i].y, path[i + 1].x, path[i + 1].y);
-          // this.animation_interval = setInterval(() => {
-          //   this.ball_step(path[i].x, path[i].y, path[i + 1].x, path[i + 1].y);
-          //   i++;
-          //   if (i == path.length - 2) {
-          //     clearInterval(this.animation_interval);
-          //   }
-          // }, 200);
         }
       }
     }
@@ -155,9 +188,6 @@ export class KulkiComponent {
     this.board[x1][y1].is_path = false;
   }
   find_path(destination: squere) {
-    console.log(this.selected_squere);
-    console.log(destination);
-
     let start: path_node = {
       visited: false,
       x: this.selected_squere.x,
@@ -350,15 +380,76 @@ export class KulkiComponent {
     if (i == 81) {
       return start;
     } else {
-      this.check();
       return visited[0];
     }
   }
   check() {
-    console.log('check');
-    //krzyz w srodku
-    //4tablice rzedzy kolumny skosy rosnoce malejace
-    this.add_balls();
+    let to_clear: squere[] = [];
+
+    for (let i = 0; i < 9; i++) {
+      if (this.board[4][i].color !== 'black') {
+        let jdown = 4,
+          jup = 4;
+        let step = 0;
+        while (step < 4) {
+          if (this.board[jdown + 1][i].color == this.board[4][i].color) {
+            jdown++;
+          }
+          if (this.board[jup - 1][i].color == this.board[4][i].color) {
+            jup--;
+          }
+          step++;
+        }
+        if (jdown - jup >= 4) {
+          this.points += jdown - jup + 1;
+          for (jup; jup <= jdown; jup++) {
+            to_clear.push(this.board[jup][i]);
+          }
+        }
+      }
+
+      if (this.board[i][4].color !== 'black') {
+        let jright = 4,
+          jleft = 4;
+        let step = 0;
+        while (step < 4) {
+          if (this.board[i][jright + 1].color == this.board[i][4].color) {
+            jright++;
+          }
+          if (this.board[i][jleft - 1].color == this.board[i][4].color) {
+            jleft--;
+          }
+          step++;
+        }
+        if (jright - jleft >= 4) {
+          this.points += jright - jleft + 1;
+          for (jleft; jleft <= jright; jleft++) {
+            to_clear.push(this.board[i][jleft]);
+          }
+        }
+      }
+    }
+
+    if (to_clear.length == 0) {
+      this.add_balls();
+    } else {
+      to_clear.forEach((element) => {
+        this.board[element.x][element.y] = {
+          color: 'black',
+          x: element.x,
+          y: element.y,
+          is_selected: false,
+          is_path: false,
+        };
+        this.empty.push({
+          color: 'black',
+          x: element.x,
+          y: element.y,
+          is_selected: false,
+          is_path: false,
+        });
+      });
+    }
   }
   add_balls() {
     if (this.empty.length <= 3) {
