5f0d2c425be37e89c85fa986cd8c9cf044f1740e
TomaszGacek <gacektomasz02@gmail.com>
2024-08-22 11:59:59 +0200

    click and slide init

--- front/GryGacek/src/app/gamesComponents/click-slide/click-slide.component.ts
+++ front/GryGacek/src/app/gamesComponents/click-slide/click-slide.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './click-slide.component.html',
   styleUrl: './click-slide.component.scss',
 })
-export class ClickSlideComponent {}
+export class ClickSlideComponent {
+  sizes: string[] = ['3x3', '4x4', '5x5', '6x6'];
+  mask_path = '/assets/images/click_and_slide/mask.jpg';
+  sunflower_path = '/assets/images/click_and_slide/sunflower.jpg';
+  balck_sheep_path = '/assets/images/click_and_slide/black_sheep.jpg';
+  paths = [this.mask_path, this.sunflower_path, this.balck_sheep_path];
+  current_path = this.paths[0];
+  size = 3;
+  set_size(size: number) {
+    this.size = size;
+    console.log(this.size);
+  }
+}
